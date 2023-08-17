1e92bf6142676d33acec4a8128df3ee749f625a9
Admin <jainamisha@gmail.com>
2023-08-17 15:48:15 +0530

    basic program

---
+++ java/hourEX.java
@@ -0,0 +1,12 @@
+public class hourEX {
+    public static void main(String[] args) {
+        int hours = 2;
+        int minutes = 5;
+        int seconds = minutes * 60;
+        int hourSeconds = hours * 60 * 60;
+
+        System.out.println("Hour to seconds : " + hourSeconds);
+        System.out.println("Minutes to second :" + seconds);
+
+    }
+}
