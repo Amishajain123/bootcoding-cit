bfd2f3aab04a4e313618f50fffbc4a9422734afa
Admin <jainamisha@gmail.com>
2023-08-19 12:17:01 +0530

    basic program

---
+++ java/array/array.java
@@ -0,0 +1,20 @@
+package array;
+
+public class array {
+    public static void main(String[] args) {
+        //Declare Array
+        int[] temp;
+        // Declare + Initialize Array
+        int[] arr = {5,9,15,2,8};
+
+        // Length of array
+        int n = arr.length;
+        // Traverse Array
+        for (int i = 0; i < n; i++){
+            if (arr[i] % 2 == 0) {
+            }
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
